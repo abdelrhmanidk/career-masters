34178689b256c8b8c8a240dc93a5ab0e82d171db
abdelrhmanidk <abdelrhmanbdlhlm@gmail.com>
2024-08-23 23:22:17 +0300

    fixed some bugs and contact dialog

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2782087333"
+    serviceWorkerVersion: "2426215618"
   }
 });
